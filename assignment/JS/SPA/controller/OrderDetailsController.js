e8b53e1c48380b8bf4167a28ecb3c1adca1bef5e
desh125 <deshtharaka@gmail.com>
2023-10-14 14:40:13 +0530

    changes

---
+++ assignment/JS/SPA/controller/OrderDetailsController.js
@@ -0,0 +1,30 @@
+$('#purchaseButton').click(function () {
+    var orderId = generateOrderId(); 
+    var customerId = $('#selectCusID').val();
+    var address = $('#customerAddress').val();
+    var date = $('#orderDate').val();
+
+    var newRow = `
+        <tr>
+            <td>${orderId}</td>
+            <td>${customerId}</td>
+            <td>${address}</td>
+            <td>${date}</td>
+            <td>
+                <button class="btn btn-warning updateOrder">Update</button>
+                <button class="btn btn-danger deleteOrder">Delete</button>
+            </td>
+        </tr>`;
+
+    $('#tb5').append(newRow);
+
+    $('.updateOrder').click(function () {
+//
+    });
+
+    $('.deleteOrder').click(function () {
+        $(this).closest('tr').remove();
+    });
+});
+
+
